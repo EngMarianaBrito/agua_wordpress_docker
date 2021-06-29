1f214f198391009b52afe11134ed8bcf874daf0f
Lia Mariana <marianabrito1791@gmail.com>
2021-06-29 00:43:20 -0300

    First_preparandoAmbiente

---
+++ agua_wordpres_docker/src/plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+function my_content_footer( $content ) {
+    if ( is_single( )) {
+        return $content . '<p>MUITO OBRIGADO PELA SUA VISITA!</p>';
+    }
+}
+
+add_filter('the_content', 'my_content_footer');
